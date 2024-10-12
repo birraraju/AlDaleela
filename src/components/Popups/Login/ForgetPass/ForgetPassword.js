3d0980d5e541e77e49e7029de13aecf0c9f236ce
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-10-12 16:18:50 +0530

    prfile login,signup,forgetpass,new pass  mobile responsive and darktheme updatoin

--- src/components/Popups/Login/ForgetPass/ForgetPassword.js
+++ src/components/Popups/Login/ForgetPass/ForgetPassword.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { X, ChevronLeft } from 'lucide-react';
 import Logo from '../../../../assets/GreenLogo.svg';
 import Input from '../Input/Input';
+import { useTheme } from '../../../Layout/ThemeContext/ThemeContext'; // Import the theme context
 
 export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNext }) {
   const [formData, setFormData] = useState({
@@ -9,6 +10,8 @@ export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNex
   });
 
   const modalRef = useRef(null);
+  const { isDarkMode } = useTheme(); // Access the dark mode state
+
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
@@ -35,13 +38,16 @@ export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNex
   };
 
   return (
-    <div className="fixed inset-10 flex items-center justify-center z-50 p-4">
+    <div className="fixed sm:inset-10 flex items-center justify-center z-50 sm:p-4 px-1">
       <div
         ref={modalRef}
-        className="bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg p-12 mb-12 rounded-2xl border border-white shadow-lg w-full max-w-sm relative"
-      >
+        className={`p-9 sm:p-12 md:p-12 mb-12 rounded-2xl border shadow-lg w-full max-w-sm relative transition-colors duration-300 ${
+          isDarkMode ? 'bg-[rgba(96,96,96,0.8)] text-white' : 'bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg text-black'
+        }`}      >
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className={`absolute top-4 right-4 hover:text-gray-800 ${
+            isDarkMode ? "text-[#FFFFFFFF] text-opacity-80" : "text-gray-800"
+          }`}
           onClick={onClose}
         >
           <X className="w-5 h-5 text-black" />
@@ -52,14 +58,14 @@ export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNex
         <div className="flex flex-col justify-between h-full">
           <div className="w-full">
             <button
-              className="flex items-center text-[14px] text-gray-600 hover:underline mb-4 mt-8 sm:mt-12"
+              className={`flex items-center text-[14px] ${isDarkMode ? 'text-gray-300' : 'text-gray-600'} hover:underline mb-4 mt-8 sm:mt-12`}
               onClick={onBackToLogin}
             >
-              <ChevronLeft className="w-6 h-6 mr-1 text-black" />
+              <ChevronLeft className={`w-6 h-6 mr-1 ${isDarkMode ? 'text-white' : 'text-black'}`} />
               Back to sign in
             </button>
-            <h2 className="text-[24px] sm:text-[28px] font-medium text-black mb-2">Forgot Password</h2>
-            <p className="text-[14px] font-omnes font-regular text-gray-600 mb-4">
+            <h2 className={`text-[24px] sm:text-[28px] font-medium ${isDarkMode ? 'text-white' : 'text-black'} mb-2`}>Forgot Password</h2>
+            <p className={`text-[14px] font-omnes font-regular text-${isDarkMode ? '[#FFFFFFCC]' : 'gray-600'} mb-4`}>
               Enter your email and we'll send you a link to reset your password
             </p>
             <form onSubmit={handleSubmit} className="space-y-4 flex-grow">
@@ -73,8 +79,8 @@ export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNex
                 type="submit"
                 className={`w-full py-3 rounded-xl transition duration-300 text-sm ${
                   formData.email
-                    ? "bg-gradient-to-r from-[#036068] via-[#596451] to-[#1199A8] text-white"
-                    : "bg-[#82828280] bg-opacity-[99%] text-white"
+                    ? 'bg-gradient-to-r from-[#036068] via-[#596451] to-[#1199A8] text-white'
+                    : `${isDarkMode ? 'bg-[white] bg-opacity-20 text-white' : 'bg-[#82828280] text-white'}`
                 }`}
                 disabled={!formData.email}
               >
@@ -82,9 +88,9 @@ export default function ForgetPassword({ onClose, onBackToLogin, onSignup, onNex
               </button>
             </form>
             <div className="mt-auto pt-4 text-center">
-              <p className="text-sm text-black text-center mt-10">
+              <p className={`text-sm text-${isDarkMode ? '[#FFFFFFCC]' : 'gray-600'} text-center mt-10`}>
                 Don't have an account?{' '}
-                <button onClick={onSignup} className="text-[#004987] font-medium underline">
+                <button onClick={onSignup} className={`text-${isDarkMode ? '[#004987]' : '[#004987]'} font-medium underline`}>
                   Sign up
                 </button>
               </p>
--- src/components/Popups/Login/NewPassword/NewPassword.js
+++ src/components/Popups/Login/NewPassword/NewPassword.js
@@ -4,6 +4,8 @@ import Logo from '../../../../assets/GreenLogo.svg';
 import Input from '../Input/Input';
 import PasswordChangeSuccess from './PasswordPopup/PasswordPopup';
 import { IoEyeOff } from "react-icons/io5";
+import { useTheme } from '../../../Layout/ThemeContext/ThemeContext'; // Import the theme context
+
 
 export default function ResetPassword({ onClose, onBackToLogin, onSignup, onPasswordSet }) {
   const [formData, setFormData] = useState({
@@ -14,6 +16,8 @@ export default function ResetPassword({ onClose, onBackToLogin, onSignup, onPass
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [showSuccessPopup, setShowSuccessPopup] = useState(false);
   const [showResetForm, setShowResetForm] = useState(true);
+  const { isDarkMode } = useTheme(); // Access the dark mode state
+
 
   const modalRef = useRef(null);
 
@@ -65,35 +69,38 @@ export default function ResetPassword({ onClose, onBackToLogin, onSignup, onPass
   }
 
   return (
-    <div className="absolute inset-10 flex items-center justify-center z-50">
+    <div className="absolute sm:inset-10 inset-0 flex items-center  justify-center z-50">
       <div
         ref={modalRef}
-        className="bg-white bg-opacity-65 backdrop-filter backdrop-blur-lg p-14 mb-10 rounded-lg shadow-xl w-full max-w-sm border border-white"
-      >
+        className={`sm:p-14 px-4 py-8 mb-10 rounded-lg  shadow-xl w-full max-w-sm  border relative transition-colors duration-300 ${
+          isDarkMode ? 'bg-[rgba(96,96,96,0.8)] text-white' : 'bg-white bg-opacity-65 backdrop-filter backdrop-blur-lg text-black'
+        }`}      >
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
           onClick={onClose}
           aria-label="Close"
         >
-          <X className="w-5 text-black" />
+          <X className="w-5 h-5 text-black" />
         </button>
-        <div className="flex justify-center mb-6">
+        <div className="flex justify-center mb-6 sm:w-auto w-60 ">
           <img src={Logo} alt="Logo" className="h-12" />
         </div>
         <div className="flex flex-col justify-between">
           <div className="w-full">
             <button
-              className="flex items-center text-[14px] font-medium text-gray-600 hover:underline mb-4"
+              className={`flex items-center text-[14px] font-medium ${
+                isDarkMode ? 'text-white text-opacity-80' : 'text-gray-600'
+              } hover:underline mb-4`}
               onClick={onBackToLogin}
             >
-              <ChevronLeft className="w-5 h-5 mr-1 text-black" />
+              <ChevronLeft className={`w-5 h-5 mr-1 ${isDarkMode ? 'text-white' : 'text-black'}`} />
               Back to sign in
             </button>
-            <h2 className="text-2xl font-semibold text-black mb-6">
+            <h2 className={`text-2xl font-semibold mb-6 ${isDarkMode ? 'text-white' : 'text-black'}`}>
               Please Enter a New Password
             </h2>
 
-            <form onSubmit={handleSubmit} className="space-y-4">
+            <form onSubmit={handleSubmit} className="space-y-4 flex-grow">
               <div className="relative">
                 <Input
                   type={showPassword ? 'text' : 'password'}
@@ -107,7 +114,7 @@ export default function ResetPassword({ onClose, onBackToLogin, onSignup, onPass
                   onClick={() => setShowPassword(!showPassword)}
                   aria-label={showPassword ? 'Hide password' : 'Show password'}
                 >
-                  <IoEyeOff className="text-2xl opacity-50 text-black" />
+                  <IoEyeOff className={`text-2xl ${isDarkMode ? 'text-white' : 'text-black'} opacity-50`} />
                 </button>
               </div>
               <div className="relative">
@@ -125,16 +132,20 @@ export default function ResetPassword({ onClose, onBackToLogin, onSignup, onPass
                     showConfirmPassword ? 'Hide confirm password' : 'Show confirm password'
                   }
                 >
-                  <IoEyeOff className="text-2xl opacity-50 text-black" />
+                  <IoEyeOff className={`text-2xl ${isDarkMode ? 'text-white' : 'text-black'} opacity-50`} />
                 </button>
               </div>
             </form>
             <button
               onClick={handleSubmit}
-              className={`w-full py-3 mt-5 mb-4 rounded-xl transition duration-300 text-sm ${
-                !isResetDisabled
-                  ? "bg-gradient-to-r from-[#036068] text-[14px] via-[#596451] to-[#1199A8] text-white"
-                  : "bg-[#828282] bg-opacity-[80%] text-white text-[14px]"
+              className={`w-full h-[40px] mt-5 mb-4 rounded-xl font-omnes transition duration-300 ${
+                isResetDisabled
+                  ? isDarkMode
+                    ? 'bg-[white] bg-opacity-20 text-white font-medium'
+                    : 'bg-[#828282] bg-opacity-80 text-white text-[14px]'
+                  : isDarkMode
+                  ? 'bg-gradient-to-r from-[#036068] via-[#596451] to-[#1199A8]'
+                  : 'bg-gradient-to-r from-[#036068] via-[#596451] to-[#1199A8]'
               }`}
               disabled={isResetDisabled}
             >
