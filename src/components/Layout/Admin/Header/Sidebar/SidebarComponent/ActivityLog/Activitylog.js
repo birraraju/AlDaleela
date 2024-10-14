6e091371f83e11bd763038fe3a27b44fbde1b032
AD <anjanadevi.iyyappan@gmail.com>
2024-10-14 12:39:08 +0530

    Admin Feedback Content Activity Dark Theme

--- src/components/Layout/Admin/Header/Sidebar/SidebarComponent/ActivityLog/Activitylog.js
+++ src/components/Layout/Admin/Header/Sidebar/SidebarComponent/ActivityLog/Activitylog.js
@@ -1,4 +1,5 @@
 import { ArrowDown } from 'lucide-react';
+import { useTheme } from "../../../../../ThemeContext/ThemeContext"; // Importing the theme context
 
 const activityLogs = [
   { userName: "User Name", emailId: "user@gmail.com", dateTime: "2024-10-11 09:22:25", ipAddress: "192.168.125.10", action: "Profile update" },
@@ -21,10 +22,15 @@ const activityLogs = [
 ];
 
 export default function UserActivityLog() {
+  const { isDarkMode } = useTheme(); // Access dark mode from theme context
+
   return (
-    <div className="bg-white p-8 rounded-lg shadow-sm h-[calc(100vh-6rem)] flex flex-col">
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-[22px] font-medium text-gray-800">User Activity Log</h2>
+<div  className={`p-8 rounded-lg shadow-sm h-[calc(100vh-6rem)] flex flex-col ${
+        isDarkMode ? "bg-[#303031] bg-opacity-90" : "bg-white "
+      } text-black backdrop-blur border-none`}>
+              <div className="flex justify-between items-center mb-6">
+              <h2 className={`text-[22px] font-medium  ${isDarkMode ? "text-[#FFFFFFCC]" : "text-gray-800"}`}>
+              User Activity Log</h2>
         <button 
           className="bg-[#3B8686] text-white px-4 py-2 rounded-lg font-medium text-sm flex items-center"
         >
@@ -32,28 +38,47 @@ export default function UserActivityLog() {
           <ArrowDown className="ml-2 h-4 w-4" />
         </button>
       </div>
-      <hr className="border-t border-gray-300 my-4" />
+      <hr className={`border-t  my-4 ${isDarkMode ? "border-[#FFFFFF] border-opacity-10" : "border-gray-300"}`} />
 
       <div className="overflow-hidden flex-grow relative">
         <div className="overflow-x-auto overflow-y-auto absolute inset-0">
           <table className="w-full border-collapse">
-            <thead className="sticky top-0 bg-white z-10">
+          <thead className={`sticky top-0   z-10 ${isDarkMode ? "bg-[#303031] " : "bg-white"}`}>
               <tr className="text-left border-b border-[#E5E7EB]">
-                <th className="pb-3 font-medium text-[14px] text-[#667085] pl-4">User Name</th>
-                <th className="pb-3 font-medium text-[14px] text-[#667085] pr-4">Email ID</th>
-                <th className="pb-3 font-medium text-[14px] text-[#667085] pr-4">Date & Time</th>
-                <th className="pb-3 font-medium text-[14px] text-[#667085] pr-4">IP Address</th>
-                <th className="pb-3 font-medium text-[14px] text-[#667085]">Action</th>
+              <th className={`pb-3 p-2 font-medium font-omnes text-[14px]  pr-2 ${isDarkMode ? "text-[#FFFFFF]" : "text-[#667085]"}`}>
+              User Name</th>
+              <th className={`pb-3 p-2 font-medium font-omnes text-[14px]  pr-2 ${isDarkMode ? "text-[#FFFFFF]" : "text-[#667085]"}`}>
+              Email ID</th>
+              <th className={`pb-3 p-2 font-medium font-omnes text-[14px]  pr-2 ${isDarkMode ? "text-[#FFFFFF]" : "text-[#667085]"}`}>
+              Date & Time</th>
+              <th className={`pb-3 p-2 font-medium font-omnes text-[14px]  pr-2 ${isDarkMode ? "text-[#FFFFFF]" : "text-[#667085]"}`}>
+              IP Address</th>
+              <th className={`pb-3 p-2 font-medium font-omnes text-[14px]  pr-2 ${isDarkMode ? "text-[#FFFFFF]" : "text-[#667085]"}`}>
+              Action</th>
               </tr>
             </thead>
             <tbody>
               {activityLogs.map((log, index) => (
-                <tr key={index} className={index % 2 === 0 ? "bg-[#D5E5DE] bg-opacity-30" : "bg-white"}>
-                  <td className="py-4 font-medium text-[14px] text-black pl-4">{log.userName}</td>
-                  <td className="py-4 font-medium text-[14px] text-black pr-4">{log.emailId}</td>
-                  <td className="py-4 font-medium text-[14px] text-black pr-4">{log.dateTime}</td>
-                  <td className="py-4 font-medium text-[14px] text-black pr-4">{log.ipAddress}</td>
-                  <td className="py-4 font-medium text-[14px] text-black pr-4">{log.action}</td>
+                <tr key={index} 
+                className={`${
+                  isDarkMode
+                    ? index % 2 === 0
+                      ? "bg-transparent"
+                      : "bg-white bg-opacity-10"
+                    : index % 2 === 0
+                    ? "bg-[#D5E5DE] bg-opacity-30"
+                    : "bg-white"
+                }`}                >
+                    <td className={`py-4 font-medium font-omnes text-[14px]  pl-2 ${isDarkMode ? "text-[#FFFFFF] text-opacity-60" : "text-black"}`}>
+                    {log.userName}</td>
+                    <td className={`py-4 font-medium font-omnes text-[14px]  pl-2 ${isDarkMode ? "text-[#FFFFFF] text-opacity-60" : "text-black"}`}>
+                    {log.emailId}</td>
+                    <td className={`py-4 font-medium font-omnes text-[14px]  pl-2 ${isDarkMode ? "text-[#FFFFFF] text-opacity-60" : "text-black"}`}>
+                    {log.dateTime}</td>
+                    <td className={`py-4 font-medium font-omnes text-[14px]  pl-2 ${isDarkMode ? "text-[#FFFFFF] text-opacity-60" : "text-black"}`}>
+                    {log.ipAddress}</td>
+                    <td className={`py-4 font-medium font-omnes text-[14px]  pl-2 ${isDarkMode ? "text-[#FFFFFF] text-opacity-60" : "text-black"}`}>
+                    {log.action}</td>
                 </tr>
               ))}
             </tbody>
