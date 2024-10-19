39292b1cf4d3b2e7338a1c7dd40246f43732b8ae
birraraju <appalaraju.b@ispatialtec.com>
2024-10-19 21:36:52 +0400

    Home button and Pan and Map on click event and Seach by catagory updaes

--- src/components/Layout/SideBar/sidebar.js
+++ src/components/Layout/SideBar/sidebar.js
@@ -10,14 +10,49 @@ import LeftArrow from '../../../assets/navigations/imagesideLeftArrow.png';
 import RightArrow from '../../../assets/navigations/imagesideRigthArrow.png'; // Fixed typo in variable name
 import DashSign from '../../../assets/navigations/image.png';
 import { useTheme } from "../ThemeContext/ThemeContext";
+import { useAuth } from "../../../Providers/AuthProvider/AuthProvider";
+import { useState } from "react";
 
 export default function Sidebar() {
   const { isDarkMode,isLangArab } = useTheme(); // Access the theme from context
-
+  const {contextMapView, initialExtent, extentHistory, setExtentHistory, currentExtentIndex, setCurrentExtentIndex} = useAuth();
+  const [panmode, setpanmode] = useState(true)
+  var handleHomeClick = () =>{
+    if(contextMapView) {
+      contextMapView.goTo({
+        center: initialExtent.center,
+        zoom: initialExtent.zoom,
+      });
+    }
+  }
+  var handleNextLevelClick = () =>{
+    if (currentExtentIndex < extentHistory.length - 1) {
+      const nextExtent = extentHistory[currentExtentIndex + 1];
+      contextMapView.goTo(nextExtent);
+      setCurrentExtentIndex(currentExtentIndex + 1);
+    }
+  }
+  var handlePrivousLevelClick = () =>{
+    if (currentExtentIndex > 0) {
+      const previousExtent = extentHistory[currentExtentIndex - 1];
+      contextMapView.goTo(previousExtent);
+      setCurrentExtentIndex(currentExtentIndex - 1);
+    }
+  }
+  const handlePanModeClick = () =>{
+    if(panmode){
+      contextMapView.surface.style.cursor = "pointer";
+      setpanmode(false)
+    }
+    else{
+      contextMapView.surface.style.cursor = "default";
+      setpanmode(true)
+    }    
+  }
   return (
     <div className={`fixed ${isLangArab? "right-4" :"left-4"}  ml-3 laptop_s:top-1/2 sm:top-64 sm:mt-2 laptop_s:mt-0 laptop_s:-translate-y-1/2 z-10 sm:flex hidden flex-col items-center space-y-2 bg-transparent p-2 rounded-full`}>
       {/* Home Button */}
-      <button className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
+      <button onClick={handleHomeClick} className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
         <img src={isDarkMode ? HomeDark : Home} alt="Home Icon" className="w-10 h-10" />
         <div className="absolute py-6 flex-1 justify-between">
           <img src={HomeSign} alt="Home Sign" className="w-6" />
@@ -37,7 +72,7 @@ export default function Sidebar() {
       </div>
 
       {/* Hand Button */}
-      <button className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
+      <button onClick={handlePanModeClick} className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
         <img src={isDarkMode ? HomeDark : Home} alt="Hand Icon" className="w-10 h-10" />
         <div className="absolute py-6 flex-1 justify-between">
           <img src={Hand} alt="Hand Sign" className="w-4 mr-1" />
@@ -45,7 +80,7 @@ export default function Sidebar() {
       </button>
 
       {/* Right Arrow Button */}
-      <button className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
+      <button onClick={handleNextLevelClick} className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
         <img src={isDarkMode ? HomeDark : Home} alt="Right Arrow Icon" className="w-10 h-10" />
         <div className="absolute py-6 flex-1 justify-between">
           <img src={RightArrow} alt="Right Arrow" className="w-3" />
@@ -53,7 +88,7 @@ export default function Sidebar() {
       </button>
 
       {/* Left Arrow Button */}
-      <button className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
+      <button onClick={handlePrivousLevelClick} className="w-12 h-12 text-white rounded-full flex items-center justify-center transition-colors duration-200">
         <img src={isDarkMode ? HomeDark : Home} alt="Left Arrow Icon" className="w-10 h-10" />
         <div className="absolute py-6 flex-1 justify-between">
           <img src={LeftArrow} alt="Left Arrow" className="w-3 mr-1" />
