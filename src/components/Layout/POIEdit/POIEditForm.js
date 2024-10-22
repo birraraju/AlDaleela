b56d91be017d0268535f8c5b0012f24495c050a9
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-10-22 09:56:46 +0530

    POI edit update

--- src/components/Layout/POIEdit/POIEditForm.js
+++ src/components/Layout/POIEdit/POIEditForm.js
@@ -7,127 +7,53 @@ import PlayThumbPOI from '../../../assets/POIEdit/POIVideoThumb.png'
 import AudioPlayPOI from '../../../assets/POIEdit/AudioPlay.svg'
 import AudioLineStylePOI from '../../../assets/POIEdit/AudioLineStyle.svg'
 
+const Component = ({ POIFormShow, setPOIUploaderShow,setIsShowEditPOI, setPOIFormShow, isEditShowPOI }) => {
 
+  if (!POIFormShow) return null;
+  
+  const renderFieldOrText = (id, label, value, inputType = "text") => {
+    return (
+      <div className="space-y-2">
+        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+        {isEditShowPOI ? (
+          inputType === "select" ? (
+            <select id={id} defaultValue={value} className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+              <option value={value}>{value}</option>
+            </select>
+          ) : (
+            <input
+              id={id}
+              defaultValue={value}
+              className="block w-full rounded-md p-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            />
+          )
+        ) : (
+          <p className="p-2 border rounded-md bg-gray-100">{value}</p>
+        )}
+      </div>
+    );
+  };
 
-
-
-const Component = ({POIFormShow,setPOIUploaderShow,setPOIFormShow}) => {
-
-  if(!POIFormShow) return null;
   return (
     <div className="w-full max-w-md bg-transparent overflow-y-auto ">
       <div className="p-2 space-y-4">
-        <div className="space-y-2">
-          <label htmlFor="organization" className="block text-sm font-medium text-gray-700">
-            Organization
-          </label>
-          <select id="organization" defaultValue="DMT" className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-            <option value="DMT">DMT</option>
-          </select>
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-            Name
-          </label>
-          <input
-            id="name"
-            defaultValue="Al Buwam"
-            className="block w-full rounded-md p-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="class" className="block text-sm font-medium text-gray-700">
-            Class
-          </label>
-          <select id="class" defaultValue="Zubara" className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-            <option value="Zubara">Zubara</option>
-          </select>
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="classD" className="block text-sm font-medium text-gray-700">
-            ClassD
-          </label>
-          <input
-            id="classD"
-            defaultValue="DMT"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="status" className="block text-sm font-medium text-gray-700">
-            Status
-          </label>
-          <select id="status" defaultValue="needs-review" className="block p-2 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-            <option value="needs-review">Needs Review</option>
-          </select>
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="comment" className="block text-sm font-medium text-gray-700">
-            Comment
-          </label>
-          <input
-            id="comment"
-            defaultValue="Imported from UAEU Atlas"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="description" className="block text-sm font-medium text-gray-700">
-            Description
-          </label>
-          <input
-            id="description"
-            defaultValue="Eastern and western"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="poems" className="block text-sm font-medium text-gray-700">
-            Poems
-          </label>
-          <input
-            id="poems"
-            defaultValue="بيت الزوم وبه ... ما جرى الاحسان بالي شوالك بحر ... ما هو ما"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 font-arabic"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="stories" className="block text-sm font-medium text-gray-700">
-            Stories
-          </label>
-          <input
-            id="stories"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="classification" className="block text-sm font-medium text-gray-700">
-            Classification
-          </label>
-          <select id="classification" defaultValue="marine" className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-            <option value="marine">Marine</option>
-          </select>
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="municipality" className="block text-sm font-medium text-gray-700">
-            Municipality
-          </label>
-          <select id="municipality" defaultValue="al-dhafra" className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-            <option value="al-dhafra">Al Dhafra</option>
-          </select>
-        </div>
-        <div className="space-y-2">
-          <label htmlFor="emirate" className="block text-sm font-medium text-gray-700">
-            Emirate
-          </label>
-          <input
-            id="emirate"
-            defaultValue="Abu Dhabi"
-            className="block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
-            {/* Photos Section */}
-            <div className="px-3 py-6 border border-none rounded-lg bg-white space-y-4">
+        {renderFieldOrText("organization", "Organization", "DMT", "select")}
+        {renderFieldOrText("name", "Name", "Al Buwam")}
+        {renderFieldOrText("class", "Class", "Zubara", "select")}
+        {renderFieldOrText("classD", "ClassD", "DMT")}
+        {renderFieldOrText("status", "Status", "Needs Review", "select")}
+        {renderFieldOrText("comment", "Comment", "Imported from UAEU Atlas")}
+        {renderFieldOrText("description", "Description", "Eastern and western")}
+        {renderFieldOrText("poems", "Poems", "بيت الزوم وبه ... ما جرى الاحسان بالي شوالك بحر ... ما هو ما")}
+        {renderFieldOrText("stories", "Stories", "")}
+        {renderFieldOrText("classification", "Classification", "Marine", "select")}
+        {renderFieldOrText("municipality", "Municipality", "Al Dhafra", "select")}
+        {renderFieldOrText("emirate", "Emirate", "Abu Dhabi")}
+
+        {/* Photos Section */}
+        <div className="px-3 py-6 border border-none rounded-lg bg-white space-y-4">
           <div>
             <h3 className="text-sm font-medium mb-2">Photos</h3>
             <div className="rounded-lg overflow-hidden">
@@ -153,7 +79,7 @@ const Component = ({POIFormShow,setPOIUploaderShow,setPOIFormShow}) => {
                   <button
                     className="absolute inset-0 m-auto bg-black/50 hover:bg-black/70 flex items-center justify-center"
                   >
-                  <img src={PlayIconPOI} className=" h-10" alt="" />
+                    <img src={PlayIconPOI} className=" h-10" alt="" />
                   </button>
                 </div>
               ))}
@@ -163,36 +89,40 @@ const Component = ({POIFormShow,setPOIUploaderShow,setPOIFormShow}) => {
           {/* Audio Section */}
           <div>
             <h3 className="text-sm font-medium mb-2">Audio</h3>
-              <div className="flex p-2 h-10 bg-gray-300 rounded-full justify-center items-center  overflow-hidden">
+            <div className="flex p-2 h-10 bg-gray-300 rounded-full justify-center items-center overflow-hidden">
               <button className=" h-8 rounded-full">
                 <img src={AudioPlayPOI} className=" h-full" alt="" />
               </button>
               <div className=" pl-3">
-              <img src={AudioLineStylePOI} alt="" />
+                <img src={AudioLineStylePOI} alt="" />
               </div>
-              </div>
+            </div>
           </div>
         </div>
-        <div className="flex justify-center space-x-8 items-center">
-                <button
-                  className="w-auto py-3 px-9 bg-transparent text-xs border border-black rounded-lg"
-                >
-                  Cancel
-                </button>
-                <button
-                  className="w-auto py-3 px-9 bg-custom-gradient text-xs border border-gray-300 rounded-lg"
-                >
-                  Update
-                </button>
-              </div>
-              <div className=" grid grid-cols-1 py-3 justify-center items-center">
-                <p className=" flex justify-center text-sm items-center">Want to share photos, videos, and audio </p>
-                <p className=" flex justify-center text-sm items-center">for this location?</p>
-                <p className=" flex justify-center text-sm items-center">please click the upload button.</p>
-              </div>
-              <div className=" flex justify-center items-center">
-                <p onClick={()=>{setPOIUploaderShow(true);setPOIFormShow(false)}} className=" cursor-pointer text-blue-500 hover:text-blue-800  underline">upload a file</p>
-              </div>
+
+        {/* Action Buttons */}
+        {isEditShowPOI && (
+          <div className="flex justify-center space-x-8 items-center">
+            <button onClick={()=>setIsShowEditPOI(false)} className="w-auto py-3 px-9 bg-transparent text-xs border border-black rounded-lg">
+              Cancel
+            </button>
+            <button onClick={()=>setIsShowEditPOI(false)} className="w-auto py-3 px-9 bg-custom-gradient text-xs border border-gray-300 rounded-lg">
+              Update
+            </button>
+          </div>
+        )}
+
+        {isEditShowPOI && (<><div className="grid grid-cols-1 py-3 justify-center items-center">
+          <p className="flex justify-center text-sm items-center">Want to share photos, videos, and audio</p>
+          <p className="flex justify-center text-sm items-center">for this location?</p>
+          <p className="flex justify-center text-sm items-center">Please click the upload button.</p>
+        </div>
+        <div className="flex justify-center items-center">
+          <p onClick={() => { setPOIUploaderShow(true); setPOIFormShow(false); }} className="cursor-pointer text-blue-500 hover:text-blue-800 underline">
+            Upload a file
+          </p>
+        </div>
+        </>)}
 
         <div className="text-sm text-gray-500 px-12">X 54.2971051, Y 24.0622842</div>
       </div>
@@ -201,4 +131,3 @@ const Component = ({POIFormShow,setPOIUploaderShow,setPOIFormShow}) => {
 };
 
 export default Component;
-
