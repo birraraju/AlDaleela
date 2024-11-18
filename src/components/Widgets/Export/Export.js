a6a85961bd5a949546a1b4769028499a09f14e76
Navaneethakrishnan85059 <navaneethakrishnan85059@gmail.com>
2024-11-18 17:35:10 +0530

    bookmark profile eportpoi  styles updated

--- src/components/Widgets/Export/Export.js
+++ src/components/Widgets/Export/Export.js
@@ -1,29 +1,44 @@
 import React, { useEffect, useRef } from "react";
 import Print from "@arcgis/core/widgets/Print";
-import './Export.css';
+import "./Export.css";
 import { useAuth } from "../../../Providers/AuthProvider/AuthProvider";
 
 const PrintComponent = ({ mapview }) => {
   const exportRef = useRef(null);
-  const {setexportWidget} = useAuth();
+  const { setexportWidget } = useAuth();
 
   useEffect(() => {
-    const observer = new MutationObserver(() => {
-      const layoutTab = document.querySelector('#exportDiv__layoutTab');
-      if (layoutTab) layoutTab.textContent = 'Export';
-  
-      const exportsTab = document.querySelector('#exportDiv__exportedFilesTab');
-      if (exportsTab) exportsTab.textContent = 'Result';
+    const buttonObserver = new MutationObserver(() => {
+      const layoutTab = document.querySelector("#exportDiv__layoutTab");
+      if (layoutTab) layoutTab.textContent = "Export";
+
+      const calciteCombobox = document.querySelector("calcite-combobox");
+      const WrapperShadow =calciteCombobox?.shadowRoot?.querySelector(".wrapper");
+      const WrapperShadowActive =calciteCombobox?.shadowRoot?.querySelector(".wrapper--active");
+      const List = document.querySelector("calcite-combobox-item");
+      const List_Active = List?.shadowRoot?.querySelector(".label--active");
+
+      if (WrapperShadow && WrapperShadowActive && List_Active) {
+        WrapperShadow.style.borderColor = "white";
+        WrapperShadow.style.height = "40px";
+        WrapperShadow.style.borderRadius = "12px";
+        WrapperShadowActive.style.outline = "none";
+
+        List_Active.style.outline = "none";
+      }
+
+      const exportsTab = document.querySelector("#exportDiv__exportedFilesTab");
+      if (exportsTab) exportsTab.textContent = "Result";
     });
-  
-    const targetNode = document.querySelector('#exportDiv');
-    if (targetNode) {
-      observer.observe(targetNode, { childList: true, subtree: true });
+
+    const bodyNode = document.querySelector("body");
+    if (bodyNode) {
+      buttonObserver.observe(bodyNode, { childList: true, subtree: true });
     }
-  
-    return () => observer.disconnect(); // Cleanup observer
+
+    // Cleanup observer on unmount
+    return () => buttonObserver.disconnect();
   }, []);
-  
 
   useEffect(() => {
     if (mapview && exportRef.current) {
@@ -31,24 +46,25 @@ const PrintComponent = ({ mapview }) => {
         const exportWidget = new Print({
           view: mapview,
           container: exportRef.current,
-          printServiceUrl: "https://maps.smartgeoapps.com/server/rest/services/AlDaleela/Print_Layout_Aldaleela/GPServer/Export%20Web%20Map",
-          allowedFormats: ["32-bit Portable Network Graphics (PNG32)","Joint Photographic Experts Group (JPG)"],
+          printServiceUrl:
+            "https://maps.smartgeoapps.com/server/rest/services/AlDaleela/Print_Layout_Aldaleela/GPServer/Export%20Web%20Map",
+          allowedFormats: [
+            "32-bit Portable Network Graphics (PNG32)",
+            "Joint Photographic Experts Group (JPG)",
+          ],
           allowedLayouts: ["Map"],
           templateOptions: {
             title: "My Print",
             //author: "Sam",
             //copyright: "My Company",
             //legendEnabled: false
-          }
+          },
         });
         setexportWidget(exportWidget);
 
-     
-
-        
         // Observe for changes in the exportRef container to remove the "Map Only" tab when it appears
         const observer = new MutationObserver(() => {
-          const liElement = document.getElementById('exportDiv__mapOnlyTab');
+          const liElement = document.getElementById("exportDiv__mapOnlyTab");
           if (liElement) {
             liElement.remove(); // Remove the <li> element directly
             observer.disconnect(); // Stop observing once the element is removed
@@ -58,11 +74,10 @@ const PrintComponent = ({ mapview }) => {
         observer.observe(exportRef.current, { childList: true, subtree: true });
 
         return () => {
-          if(exportRef.current){
-              //printWidget.destroy();
-              exportRef.current = null;
+          if (exportRef.current) {
+            //printWidget.destroy();
+            exportRef.current = null;
           }
-          
         };
       } catch (error) {
         console.error("Error initializing print widget:", error);
@@ -70,7 +85,13 @@ const PrintComponent = ({ mapview }) => {
     }
   }, [mapview]);
 
-  return <div className=" sm:-mt-[500px]   laptop_s:-mt-[450px] -mt-[420px] Export" id="exportDiv" ref={exportRef} />;
+  return (
+    <div
+      className=" sm:-mt-[500px]   laptop_s:-mt-[450px] -mt-[420px] Export"
+      id="exportDiv"
+      ref={exportRef}
+    />
+  );
 };
 
 export default PrintComponent;
