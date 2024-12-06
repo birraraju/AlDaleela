7321a869db75ed88ff04ef0fb2713b9a57bf462c
Haseebabdu <abdulhaseebah0702@gmail.com>
2024-12-06 17:06:50 +0530

    sheet3 opened bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,8 @@ export default {
         screens: {
           mobile_s: "100px",
           mobile_m: "375px",
-          mobile_m_3: "413px",
+          mobile_m_3:"393px",
+          mobile_m_4:"413px",
           mobile_l: "425px",tab:"768px",
           tab_s:"800px",
           tab_m:"820px",
